d7b98034cb8fed8970710c4c675285186aef4463
Sri Patmawati <sriwatiaz@gmail.com>
2018-05-06 08:44:53 +0800

    Instance, buat atribut dan panggil method

--- src/praktek03/SepatuAksi.java
+++ src/praktek03/SepatuAksi.java
@@ -1,6 +1,11 @@
 package praktek03;
 public class SepatuAksi {
     public static void main(String[] args) {
+        Sepatu s = new Sepatu();
+        s.merk="Belalang";
+        s.warna="Biru";
+        s.harga=5000;
         
+        s.cetakInfo();
     }
 }
