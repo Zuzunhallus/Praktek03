017c6370c96562b4547b2192bc053cb6fdfba410
Sri Patmawati <sriwatiaz@gmail.com>
2018-05-06 09:16:35 +0800

    menggunakan method get

--- src/praktek03/SepatuAksi.java
+++ src/praktek03/SepatuAksi.java
@@ -8,5 +8,12 @@ public class SepatuAksi {
         s.setHarga(5000);
         
         s.cetakInfo();
+        
+        System.out.print("Merknya \t: ");
+        System.out.println(s.getMerk());
+        System.out.print("Warnanya \t: ");
+        System.out.println(s.getWarna());
+        System.out.print("Harganya \t: ");
+        System.out.println(s.getHarga());
     }
 }
